4480fa69aa45cfdab3bb9d9d97beeae740b74f23
bened <bezunyl@gmail.com>
2023-04-29 02:48:01 +0200

    first implementation of a basic scrape for the current menu

---
+++ README.md
@@ -0,0 +1,7 @@
+# Mensa essWerk Menu
+
+An Android application which allows a quick and customizable insight into the daily menu of the mensa essWerk.
+
+## Features
+- Food options with pricing
+- Customizable filter for allergies
--- app/src/main/java/com/example/mensaesswerkmenu/MainActivity.kt
+++ app/src/main/java/com/example/mensaesswerkmenu/MainActivity.kt
@@ -2,10 +2,60 @@ package com.example.mensaesswerkmenu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import it.skrape.core.htmlDocument
+import it.skrape.fetcher.HttpFetcher
+import it.skrape.fetcher.extractIt
+import it.skrape.fetcher.skrape
+import it.skrape.selects.html5.div
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FetchData()
     }
+
+    data class MensaItem(var name: String = "DefaultName", var price: String = "0.0")
+    data class MensaMenu(var date: String = "", var items: MutableList<MensaItem> = mutableListOf())
+    fun FetchData() {
+        val site = skrape(HttpFetcher) {
+            request {
+                url = "https://www.swffm.de/essen-trinken/speiseplaene/mensa-esswerk"
+            }
+
+            extractIt<MensaMenu> { results ->
+                htmlDocument {
+                    val menu = findFirst(".speiseplan") {
+                        findFirst(".panel-heading"){
+                            results.date = text
+                        }
+
+                        findAll("tr"){
+                            this.forEach {
+                                var item = MensaItem()
+
+                                it.findFirst(".menu_name") {
+                                    item.name = this.text;
+                                }
+
+                                it.findFirst(".col-md-2") {
+                                    findFirst("strong") {
+                                        item.price = this.text
+                                    }
+                                }
+
+                                results.items.add(item)
+                            }
+                        }
+
+                    }
+                }
+
+                Log.d("Scraper", "Done.")
+            }
+        }
+    }
+
 }
